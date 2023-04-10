42335bc5ce06b9e7fb50230519509066e2248ca3
Andyyy429 <anderson.gibbs@my.uwi.edu>
2023-04-10 22:33:10 +0000

    Meet Team Section and navbar updated

---
+++ JAVASCRIPT/Index/meetTeam.js
@@ -0,0 +1,33 @@
+function show() {
+    let readMore = document.querySelector("#dots");
+    let moreText = document.querySelector("#more");
+    let readLess = document.querySelector("#less");
+
+    if (moreText.style.display === "none") {
+        moreText.style.display = "inline";
+        readMore.style.display = "none";
+        readLess.style.display = "inline"
+    }
+    else {
+        moreText.style.display = "none";
+        readMore.style.display = "inline"
+        readLess.style.display = "none";
+    }
+}
+
+function showMore() {
+    let readMore2 = document.querySelector("#dots2");
+    let moreText2 = document.querySelector("#more2");
+    let readLess2 = document.querySelector("#less2");
+
+    if (moreText2.style.display === "none") {
+        moreText2.style.display = "inline";
+        readMore2.style.display = "none";
+        readLess2.style.display = "inline"
+    }
+    else {
+        moreText2.style.display = "none";
+        readMore2.style.display = "inline"
+        readLess2.style.display = "none";
+    }
+}
