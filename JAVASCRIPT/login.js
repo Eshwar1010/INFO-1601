8d124bd26a544e23aa3f1105957a373848d2811e
Jael <123189873+CrixandJam@users.noreply.github.com>
2023-04-13 17:52:21 -0400

    Update login.js

--- JAVASCRIPT/login.js
+++ JAVASCRIPT/login.js
@@ -7,7 +7,7 @@ fetch(url)
  let email = data.email;
  let password = data.password; 
  
- login(username, password);
+ <!-- login(username, password);
   })
   .catch(err => {
   
@@ -26,3 +26,68 @@ fetch(url)
   }
   console.log(json.sheet1S);
 });
+ -->
+
+//Here
+let email;
+let username;
+let password;
+let apiUrl = 'https://api.sheety.co/8f8d5d5e-c6f3-4b76-a9e4-f4f4b4c7d88d';
+
+// Check valid
+function checkValidity() {
+  // Check if email is valid
+  if (validateEmail(email)) {
+    // Check if username is taken
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Check if username is taken
+        if (data.find(y => y.username === username)) {
+          alert('Username is already taken!');
+        } else if (checkPasswordStrength(password) < 5) {
+          // Check if password is weak
+          alert('Password is too weak!');
+        } else {
+          // Send data to Google Sheets using Sheety API
+          const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, username, password })
+          };
+          fetch(apiUrl, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+              alert('Sign up successful!');
+            });
+        }
+      });
+  } else {
+    alert('Email is not valid!');
+  }
+}
+
+// Validate email
+function validateEmail(email) {
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Check password strength
+function checkPasswordStrength(password) {
+  let strength = 0;
+  if (password.length < 6) return strength;
+  if (password.match(/[a-z]+/)) strength++;
+  if (password.match(/[A-Z]+/)) strength++;
+  if (password.match(/[0-9]+/)) strength++;
+  if (password.match(/[$@#&!]+/)) strength++;
+  return strength;
+}
+
+// Handle Login
+function LoginForm() {
+  email = document.getElementById('email').value;
+  username = document.getElementById('username').value;
+  password = document.getElementById('password').value;
+  checkValidity();
+}
